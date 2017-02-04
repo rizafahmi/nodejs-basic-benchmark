bf24e3862e1217676952c7016cd5f1cd1ccec092
Riza Fahmi <rizafahmi@gmail.com>
2017-02-04 18:59:17 +0700

    Benchmarking function, anonymous function and nothing

---
+++ index.js
@@ -0,0 +1,30 @@
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite
+
+function add(a, b) {
+  return a + b
+}
+
+const addAnon = (a, b) => {
+  return a + b
+}
+
+suite.add('nothing', () => {
+  // do nothing
+})
+.add('add', () => {
+  1 + 2
+})
+.add('add with function', () => {
+  add(1, 2)
+})
+.add('add with anonym function', () => {
+  addAnon(1, 2)
+})
+.on('cycle', (event) => {
+  console.log(String(event.target))
+})
+.on('complete', () => {
+  console.log('Benchmark finish')
+})
+.run({ 'async': true })
